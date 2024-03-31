63454d44495cbcac89c427200f2b740a865b6cb1
Serhii Matkobozhyk <megawacko94@gmail.com>
2024-03-31 19:51:45 +0300

    finished hw18

--- tests/hw18/hw18.spec.js
+++ tests/hw18/hw18.spec.js
@@ -1,29 +1,4 @@
 import { test, expect } from '@playwright/test';
-import { SignUpPage } from './SignUpPage';
-import { skip } from 'node:test';
-
-// // Locators
-// const signUpBtn = page.getByRole('button', { name: 'Sign up' });
-// const nameInput =  page.locator('#signupName');
-// const lastNameInput = page.locator('#signupLastName');
-// const emailInput = page.locator('#signupEmail');
-// const passwordInput = page.getByLabel('Password', { exact: true });
-// const reenterPasswordInput = page.getByLabel('Re-enter password');
-// const registerBtn = page.getByRole('button', { name: 'Register' });
-// const myProfileBtn = page.getByRole('button', { name: 'My profile' });
-// const emptyNameError = page.getByText('Name required');
-// const emptyLastNameError = page.getByText('Last name required');
-// const emptyEmailError = page.getByText('Email required');
-// const emptyPasswordError = page.getByText('Password required');
-// const invalidNameError = page.getByText('Name is invalid');
-// const invalidLastNameError = page.getByText('Last name is invalid');
-// const invalidEmailError = page.getByText('Email is incorrect');
-// const nameLengthValidationError = page.getByText('Name has to be from 2 to 20 characters long');
-// const invalidPasswordError = page.getByText('Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter');
-// const lastNameLengthValidationError = page.getByText('Last name has to be from 2 to 20 characters long');
-// const settingsBtn = page.getByRole('link', { name: 'Settings' });
-// const removeAccountBtn = page.getByRole('button', { name: 'Remove my account' });
-// const confirmRemoveAccountBtn = page.getByRole('button', { name: 'Remove' });
 
 // Test data
 const errorBorderColorCSS = 'rgb(220, 53, 69)';
@@ -42,181 +17,226 @@ const minimumLengthLastName = 'te';
 const maximumLengthLastName = 'test123test123test12';
 const longLastName = 'test123test123test123';
 const invalidEmails = ['test', 'test@', 'test@test', 'test@test.'];
-
-
+const invalidPasswords = ['qwertyu', 'Qwerty1', 'Qwertyui', 'QwertyuiQwertyu1'];
 
 test.describe('Sign Up Positive Flow', ()=>{
     
     test.beforeEach(async ({ page })=>{
-        await page.goto('');
-    })
+        await page.goto(''); 
+    });
 
     // Delete test user after each successful sign up
     test.afterEach(async ({page})=>{
-        const signUpPage = new SignUpPage(page);
-        await signUpPage.settingsBtn.click();
-        await signUpPage.removeAccountBtn.click();
-        await signUpPage.confirmRemoveAccountBtn.click();
-    })
+        const settingsBtn = page.getByRole('link', { name: 'Settings' });
+        const removeAccountBtn = page.getByRole('button', { name: 'Remove my account' });
+        const confirmRemoveAccountBtn = page.getByRole('button', { name: 'Remove' });
+        await settingsBtn.click();
+        await removeAccountBtn.click();
+        await confirmRemoveAccountBtn.click();
+    });
 
     test('Should be successfully signed up', async ({page})=>{
-        const signUpPage = new SignUpPage(page);
+        
+        const signUpBtn = page.getByRole('button', { name: 'Sign up' });
+        const nameInput =  page.locator('#signupName');
+        const lastNameInput = page.locator('#signupLastName');
+        const emailInput = page.locator('#signupEmail');
+        const passwordInput = page.getByLabel('Password', { exact: true });
+        const reenterPasswordInput = page.getByLabel('Re-enter password');
+        const registerBtn = page.getByRole('button', { name: 'Register' });
+        const myProfileBtn = page.getByRole('button', { name: 'My profile' });
 
-        await signUpPage.signUpBtn.click();
-        await signUpPage.nameInput.fill(testName);
-        await signUpPage.lastNameInput.fill(testLastName);
-        await signUpPage.emailInput.fill(testEmail);
-        await signUpPage.passwordInput.fill(testPassword);
-        await signUpPage.reenterPasswordInput.fill(testPassword);
-        await signUpPage.registerBtn.click();
+        await signUpBtn.click();
+        await nameInput.fill(testName);
+        await lastNameInput.fill(testLastName);
+        await emailInput.fill(testEmail);
+        await passwordInput.fill(testPassword);
+        await reenterPasswordInput.fill(testPassword);
+        await registerBtn.click();
 
-        await expect(signUpPage.myProfileBtn).toBeVisible();
-    })
-})
+        await expect(myProfileBtn).toBeVisible();
+    });
+});
 
 test.describe('Input fields validation', ()=>{
+    
     test.beforeEach(async ({page})=>{
         await page.goto('');
-        const signUpPage = new SignUpPage(page);
-        await signUpPage.signUpBtn.click();
-    })
+        const signUpBtn = page.getByRole('button', { name: 'Sign up' });
+        await signUpBtn.click();
+    });
 
     test.afterEach(async ({page})=> {
-        const signUpPage = new SignUpPage(page);
-        await expect(signUpPage.registerBtn).toBeDisabled();
-    })
+        const registerBtn = page.getByRole('button', { name: 'Register' });
+        await expect(registerBtn).toBeDisabled();
+    });
 
-    test('"Name" input field validation', async ({page})=>{
-        const signUpPage = new SignUpPage(page);
-// test.step.afterEach
-        // Empty name field validation
-        await signUpPage.nameInput.focus();
-        await signUpPage.nameInput.blur();
+    test('Name input field validation', async ({page})=>{
+
+        const nameInput =  page.locator('#signupName');
+        const emptyNameError = page.getByText('Name required');
+        const invalidNameError = page.getByText('Name is invalid');
+        const nameLengthValidationError = page.getByText('Name has to be from 2 to 20 characters long');
+
+        await test.step('Empty name field validation', async ()=>{
+            await nameInput.focus();
+            await nameInput.blur();
+            await expect(emptyNameError).toBeVisible();
+            await expect(nameInput).toHaveCSS('border-color', errorBorderColorCSS);
+
+            await nameInput.fill(testName);
+            await expect(emptyNameError).toBeHidden();
+        });
         
- // TO AFTER EACH??       // Border of input field is red
-        await expect(signUpPage.nameInput).toHaveCSS('border-color', errorBorderColorCSS);
+        await test.step('Invalid name validation', async ()=>{
+            await nameInput.fill(invalidName);
+            await expect(invalidNameError).toBeVisible();
+            await expect(nameInput).toHaveCSS('border-color', errorBorderColorCSS);
 
-        await expect(signUpPage.emptyNameError).toBeVisible();
-        await signUpPage.nameInput.fill(testName);
-        await expect(signUpPage.emptyNameError).toBeHidden();
+            await nameInput.fill(testName);
+            await expect(invalidNameError).toBeHidden();
+        });
 
-        // Invalid name validation
-        await signUpPage.nameInput.fill(invalidName);
-        await expect(signUpPage.invalidNameError).toBeVisible();
-        await signUpPage.nameInput.fill(testName);
-        await expect(signUpPage.invalidNameError).toBeHidden();
+        await test.step('Invalid length validation', async ()=>{
+            await nameInput.fill(shortName);
+            await expect(nameLengthValidationError).toBeVisible();
+            await expect(nameInput).toHaveCSS('border-color', errorBorderColorCSS);
 
-        // Invalid length validation
-        await signUpPage.nameInput.fill(shortName);
-        await expect(signUpPage.nameLengthValidationError).toBeVisible();
-        await signUpPage.nameInput.fill(minimumLengthName);
-        await expect(signUpPage.nameLengthValidationError).toBeHidden();
-        await signUpPage.nameInput.fill(maximumLengthName);
-        await expect(signUpPage.nameLengthValidationError).toBeHidden();
-        await signUpPage.nameInput.fill(longName);
-        await expect(signUpPage.nameLengthValidationError).toBeVisible();
-    })
+            await nameInput.fill(minimumLengthName);
+            await expect(nameLengthValidationError).toBeHidden();
+
+            await nameInput.fill(maximumLengthName);
+            await expect(nameLengthValidationError).toBeHidden();
+
+            await nameInput.fill(longName);
+            await expect(nameLengthValidationError).toBeVisible();
+            await expect(nameInput).toHaveCSS('border-color', errorBorderColorCSS);
+        });
+    });
 
     test('Last name input field validation', async ({page})=>{
-        const signUpPage = new SignUpPage(page);
-        // Empty Last name field validation
-        await signUpPage.lastNameInput.focus();
-        await signUpPage.lastNameInput.blur();
 
-        // Border of input field is red
-        await expect(signUpPage.lastNameInput).toHaveCSS('border-color', errorBorderColorCSS);
+        const lastNameInput = page.locator('#signupLastName');
+        const emptyLastNameError = page.getByText('Last name required');
+        const invalidLastNameError = page.getByText('Last name is invalid');
+        const lastNameLengthValidationError = page.getByText('Last name has to be from 2 to 20 characters long');
 
-        await expect(signUpPage.emptyLastNameError).toBeVisible();
-        await signUpPage.lastNameInput.fill(testLastName);
-        await expect(signUpPage.emptyLastNameError).toBeHidden();
+        await test.step('Empty Last name field validation', async ()=>{
+            await lastNameInput.focus();
+            await lastNameInput.blur();
+            await expect(emptyLastNameError).toBeVisible();
+            await expect(lastNameInput).toHaveCSS('border-color', errorBorderColorCSS);
 
-        // Invalid Last name validation
-        await signUpPage.lastNameInput.fill(invalidLastName);
-        await expect(signUpPage.invalidLastNameError).toBeVisible();
-        await signUpPage.lastNameInput.fill(testLastName);
-        await expect(signUpPage.invalidLastNameError).toBeHidden();
+            await lastNameInput.fill(testLastName);
+            await expect(emptyLastNameError).toBeHidden();
+        });
 
-        // Invalid length validation
-        await signUpPage.lastNameInput.fill(shortLastName);
-        await expect(signUpPage.lastNameLengthValidationError).toBeVisible();
-        await signUpPage.lastNameInput.fill(minimumLengthLastName);
-        await expect(signUpPage.lastNameLengthValidationError).toBeHidden();
-        await signUpPage.lastNameInput.fill(maximumLengthLastName);
-        await expect(signUpPage.lastNameLengthValidationError).toBeHidden();
-        await signUpPage.lastNameInput.fill(longLastName);
-        await expect(signUpPage.lastNameLengthValidationError).toBeVisible();
-    })
+        await test.step('Invalid Last name validation', async ()=>{
+            await lastNameInput.fill(invalidLastName);
+            await expect(invalidLastNameError).toBeVisible();
+            await expect(lastNameInput).toHaveCSS('border-color', errorBorderColorCSS);
+
+            await lastNameInput.fill(testLastName);
+            await expect(invalidLastNameError).toBeHidden();
+        });
+
+        await test.step('Invalid length validation', async ()=>{
+            await lastNameInput.fill(shortLastName);
+            await expect(lastNameLengthValidationError).toBeVisible();
+            await expect(lastNameInput).toHaveCSS('border-color', errorBorderColorCSS);
+
+            await lastNameInput.fill(minimumLengthLastName);
+            await expect(lastNameLengthValidationError).toBeHidden();
+
+            await lastNameInput.fill(maximumLengthLastName);
+            await expect(lastNameLengthValidationError).toBeHidden();
+
+            await lastNameInput.fill(longLastName);
+            await expect(lastNameLengthValidationError).toBeVisible();
+            await expect(lastNameInput).toHaveCSS('border-color', errorBorderColorCSS);
+        });
+    });
 
     test('Email input field validation', async ({page})=>{
-        const signUpPage = new SignUpPage(page);
-        // Empty Email field validation
-        await signUpPage.emailInput.focus();
-        await signUpPage.emailInput.blur();
 
-        // Border of input field is red
-        await expect(signUpPage.emailInput).toHaveCSS('border-color', errorBorderColorCSS);
+        const emailInput = page.locator('#signupEmail');
+        const emptyEmailError = page.getByText('Email required');
+        const invalidEmailError = page.getByText('Email is incorrect');
 
-        await expect(signUpPage.emptyEmailError).toBeVisible();
-        await signUpPage.emailInput.fill(testEmail);
-        await expect(signUpPage.emptyEmailError).toBeHidden();
+        await test.step('Empty Email field validation', async ()=>{
+            await emailInput.focus();
+            await emailInput.blur();
+            await expect(emptyEmailError).toBeVisible();
+            await expect(emailInput).toHaveCSS('border-color', errorBorderColorCSS);
 
-        // Incorrect Email validation
+            await emailInput.fill(testEmail);
+            await expect(emptyEmailError).toBeHidden();
+        });
 
-        for (let i = 0; i < invalidEmails.length; i++) {
-            await signUpPage.emailInput.fill(invalidEmails[i]);
-            await expect(signUpPage.invalidEmailError).toBeVisible();
-        }
-
-        await signUpPage.emailInput.fill(testEmail);
-        await expect(signUpPage.invalidEmailError).toBeHidden();
-    })
+        await test.step('Incorrect Email validation', async ()=>{
+            for (let i = 0; i < invalidEmails.length; i++) {
+                await emailInput.fill(invalidEmails[i]);
+                await expect(invalidEmailError).toBeVisible();
+                await expect(emailInput).toHaveCSS('border-color', errorBorderColorCSS);
+            }
     
-    test('"Password" input field validation', async ({page})=>{
-        const signUpPage = new SignUpPage(page);
-        // Empty Password field validation
-        await signUpPage.passwordInput.focus();
-        await signUpPage.passwordInput.blur();
-
-        // Border of input field is red
-        await expect(signUpPage.passwordInput).toHaveCSS('border-color', errorBorderColorCSS);
-
-        await expect(signUpPage.emptyPasswordError).toBeVisible();
-
-        // Invalid Password field validation
-        await signUpPage.passwordInput.fill('qwertyu');
-        await expect(signUpPage.invalidPasswordError).toBeVisible();
-        await signUpPage.passwordInput.fill('Qwerty1');
-        await expect(signUpPage.invalidPasswordError).toBeVisible();
-        await signUpPage.passwordInput.fill('Qwertyui');
-        await expect(signUpPage.invalidPasswordError).toBeVisible();
-        await signUpPage.passwordInput.fill('QwertyuiQwertyu1');
-        await expect(signUpPage.invalidPasswordError).toBeVisible();
-        await signUpPage.passwordInput.fill('Qwertyui1');
-        await expect(signUpPage.invalidPasswordError).toBeHidden();
-    })
+            await emailInput.fill(testEmail);
+            await expect(invalidEmailError).toBeHidden();
+        });
+    });
     
-    test,skip('"Re-enter password" input field validation', async ({page})=>{
-        const passwordInput = page.getByLabel('Re-enter password', { exact: true });
-        const invalidPWvalidationError = 'Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter';
-        // Empty Password field validation
-        await passwordInput.focus();
-        await passwordInput.blur();
+    test('Password input field validation', async ({page})=>{
 
-        // Border of input field is red
-        await expect(passwordInput).toHaveCSS('border-color', 'rgb(220, 53, 69)');
+        const passwordInput = page.getByLabel('Password', { exact: true });
+        const emptyPasswordError = page.getByText('Password required');
+        const invalidPasswordError = page.getByText('Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter');
 
-        await expect(page.getByText('Re-enter password required')).toBeVisible();
+        await test.step('Empty Password field validation', async ()=>{
+            await passwordInput.focus();
+            await passwordInput.blur();
+            await expect(emptyPasswordError).toBeVisible();
+            await expect(passwordInput).toHaveCSS('border-color', errorBorderColorCSS);
 
-        // Invalid Password field validation
-        await passwordInput.fill('qwertyu');
-        await expect(page.getByText(`${invalidPWvalidationError}`)).toBeVisible();
-        await passwordInput.fill('Qwerty1');
-        await expect(page.getByText(`${invalidPWvalidationError}`)).toBeVisible();
-        await passwordInput.fill('Qwertyui');
-        await expect(page.getByText(`${invalidPWvalidationError}`)).toBeVisible();
-        await passwordInput.fill('QwertyuiQwertyu1');
-        await expect(page.getByText(`${invalidPWvalidationError}`)).toBeVisible();
-        await passwordInput.fill('Qwertyui1');
-        await expect(page.getByText(`${invalidPWvalidationError}`)).toBeHidden();
-    })
-})
+            await passwordInput.fill(testPassword);
+            await expect(emptyPasswordError).toBeHidden();
+        });
+        
+        await test.step('Invalid Password field validation', async ()=>{
+            for (let i = 0; i < invalidPasswords.length; i++) {
+                await passwordInput.fill(invalidPasswords[i]);
+                await expect(invalidPasswordError).toBeVisible();
+                await expect(passwordInput).toHaveCSS('border-color', errorBorderColorCSS);
+            }
+    
+            await passwordInput.fill(testPassword);
+            await expect(invalidPasswordError).toBeHidden();
+        });
+    });
+    
+    test('Re-enter password input field validation', async ({page})=>{
+        const reenterPasswordInput = page.getByLabel('Re-enter password', { exact: true });
+        const emptyReenterPasswordError = page.getByText('Re-enter password required');
+        const invalidReenterPasswordError = page.getByText('Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter');
+
+        await test.step('Empty Re-enter password field validation', async ()=>{
+            await reenterPasswordInput.focus();
+            await reenterPasswordInput.blur();
+            await expect(emptyReenterPasswordError).toBeVisible();
+            await expect(reenterPasswordInput).toHaveCSS('border-color', errorBorderColorCSS);
+
+            await reenterPasswordInput.fill(testPassword);
+            await expect(emptyReenterPasswordError).toBeHidden();
+        });
+        
+        await test.step('Invalid Re-enter password field validation', async ()=>{
+            for (let i = 0; i < invalidPasswords.length; i++) {
+                await reenterPasswordInput.fill(invalidPasswords[i]);
+                await expect(invalidReenterPasswordError).toBeVisible();
+                await expect(reenterPasswordInput).toHaveCSS('border-color', errorBorderColorCSS);
+            }
+    
+            await reenterPasswordInput.fill(testPassword);
+            await expect(invalidReenterPasswordError).toBeHidden();
+        });
+    });
+});
