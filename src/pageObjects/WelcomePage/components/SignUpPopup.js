b85ec00b3f59e09a7a278c4c2af22ae745527725
Serhii Matkobozhyk <megawacko94@gmail.com>
2024-04-03 19:13:43 +0300

    finally fixed hw19

--- src/pageObjects/WelcomePage/components/SignUpPopup.js
+++ src/pageObjects/WelcomePage/components/SignUpPopup.js
@@ -1,44 +1,25 @@
 import BaseComponent from "../../../components/BaseComponent.js";
 
 export class SignUpPopup extends BaseComponent {
-    _nameInput
-    _lastNameInput
-    _emailInput
-    _passwordInput
-    _reenterPasswordInput
-    _registerBtn
-    _emptyNameError
-    _invalidNameError
-    _nameLengthValidationError
-    _emptyLastNameError
-    _invalidLastNameError
-    _lastNameLengthValidationError
-    _emptyEmailError
-    _invalidEmailError
-    _emptyPasswordError
-    _invalidPasswordError
-    _emptyReenterPasswordError
-    _invalidReenterPasswordError
+    _nameInputSelector = '#signupName';
+    _lastNameInputSelector = '#signupLastName';
+    _emailInputSelector = '#signupEmail';
+    _passwordInputSelector = '#signupPassword';
+    _reenterPasswordInputSelector = '#signupRepeatPassword';
+    _registerBtnSelector = { name: 'Register' };
 
     constructor(page) {
         super(page, page.locator('app-signup-modal'));
-        this.nameInput =  page.locator('#signupName');
-        this.lastNameInput = page.locator('#signupLastName');
-        this.emailInput = page.locator('#signupEmail');
-        this.passwordInput = page.getByLabel('Password', { exact: true });
-        this.reenterPasswordInput = page.getByLabel('Re-enter password');
-        this.registerBtn = page.getByRole('button', { name: 'Register' });
-        this.emptyNameError = page.getByText('Name required');
-        this.invalidNameError = page.getByText('Name is invalid');
-        this.nameLengthValidationError = page.getByText('Name has to be from 2 to 20 characters long');
-        this.emptyLastNameError = page.getByText('Last name required');
-        this.invalidLastNameError = page.getByText('Last name is invalid');
-        this.lastNameLengthValidationError = page.getByText('Last name has to be from 2 to 20 characters long');
-        this.emptyEmailError = page.getByText('Email required');
-        this.invalidEmailError = page.getByText('Email is incorrect');
-        this.emptyPasswordError = page.getByText('Password required');
-        this.invalidPasswordError = page.getByText('Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter');
-        this.emptyReenterPasswordError = page.getByText('Re-enter password required');
-        this.invalidReenterPasswordError = page.getByText('Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter');
+        this.nameInput =  this.container.locator(this._nameInputSelector);
+        this.lastNameInput = this.container.locator(this._lastNameInputSelector);
+        this.emailInput = this.container.locator(this._emailInputSelector);
+        this.passwordInput = this.container.locator(this._passwordInputSelector);
+        this.reenterPasswordInput = this.container.locator(this._reenterPasswordInputSelector);
+        this.registerBtn = this.container.getByRole('button', this._registerBtnSelector);
+        this.nameValidationError = this.container.locator(`${this._nameInputSelector} + .invalid-feedback`);
+        this.lastNameValidationError = this.container.locator(`${this._lastNameInputSelector} + .invalid-feedback`);
+        this.emailValidationError = this.container.locator(`${this._emailInputSelector} + .invalid-feedback`);
+        this.passwordValidationError = this.container.locator(`${this._passwordInputSelector} + .invalid-feedback`);
+        this.reenterPasswordValidationError = this.container.locator(`${this._reenterPasswordInputSelector} + .invalid-feedback`);
     }
 }
--- tests/auth/signUp.spec.js
+++ tests/auth/signUp.spec.js
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test';
 import {WelcomePage} from '../../src/pageObjects/WelcomePage/WelcomePage.js';
 import {GaragePage} from '../../src/pageObjects/GaragePage/GaragePage.js';
+import {NavBar} from '../../src/pageObjects/GaragePage/components/NavBar.js';
+import {RemoveUserPopup} from '../../src/pageObjects/GaragePage/SettingsPage/components/RemoveUserPopup.js';
 import * as testData from '../../src/data/testData.js';
 
 test.describe('Auth', () => {
@@ -17,16 +19,22 @@ test.describe('Auth', () => {
 
         test.describe('Positive flow', () => {
             let garagePage
+            let navBar
+            let settingsPage
+            let removeUserPopup
 
             test.beforeEach(async ({page}) => {
                 garagePage = new GaragePage(page);
+                navBar = new NavBar(page);
+                removeUserPopup = new RemoveUserPopup(page)
             });
 
             // Delete test user after each successful sign up
             test.afterEach(async () => {
-                await garagePage.clickSettingsBtn();
-                await garagePage.clickRemoveAccountBtn();
-                await garagePage.clickConfirmRemoveAccountBtn();
+                settingsPage = await navBar.clickSettingsBtn();
+                await navBar.clickSettingsBtn();
+                await settingsPage.clickRemoveAccountBtn();
+                await removeUserPopup.clickConfirmRemoveAccountBtn();
             });
 
             test('user should be able to sign up', async () => {
@@ -44,45 +52,44 @@ test.describe('Auth', () => {
 
         test.describe('Negative flow', () => {
             test.describe('Input fields validation', () => {
-            
-                test.afterEach(async () => {
-                    await expect(signUpPopup.registerBtn).toBeDisabled();
-                });
-            
                 test('Name input field validation', async () => {
                     await test.step('Empty name field validation', async () => {
                         await signUpPopup.nameInput.focus();
                         await signUpPopup.nameInput.blur();
-                        await expect(signUpPopup.emptyNameError).toBeVisible();
+                        await expect(signUpPopup.nameValidationError).toHaveText('Name required');
                         await expect(signUpPopup.nameInput).toHaveCSS('border-color', testData.errorBorderColorCSS);
+                        await expect(signUpPopup.registerBtn, "Register button should be disabled").toBeDisabled();
             
                         await signUpPopup.nameInput.fill(testData.testName);
-                        await expect(signUpPopup.emptyNameError).toBeHidden();
+                        await expect(signUpPopup.nameValidationError).toBeHidden();
                     });
                     
                     await test.step('Invalid name validation', async ()=>{
                         await signUpPopup.nameInput.fill(testData.invalidName);
-                        await expect(signUpPopup.invalidNameError).toBeVisible();
+                        await expect(signUpPopup.nameValidationError).toHaveText('Name is invalid');
                         await expect(signUpPopup.nameInput).toHaveCSS('border-color', testData.errorBorderColorCSS);
+                        await expect(signUpPopup.registerBtn, 'Register button should be disabled').toBeDisabled();
             
                         await signUpPopup.nameInput.fill(testData.testName);
-                        await expect(signUpPopup.invalidNameError).toBeHidden();
+                        await expect(signUpPopup.nameValidationError).toBeHidden();
                     });
             
                     await test.step('Invalid length validation', async ()=>{
                         await signUpPopup.nameInput.fill(testData.shortName);
-                        await expect(signUpPopup.nameLengthValidationError).toBeVisible();
+                        await expect(signUpPopup.nameValidationError).toHaveText('Name has to be from 2 to 20 characters long');
                         await expect(signUpPopup.nameInput).toHaveCSS('border-color', testData.errorBorderColorCSS);
+                        await expect(signUpPopup.registerBtn, 'Register button should be disabled').toBeDisabled();
             
                         await signUpPopup.nameInput.fill(testData.minimumLengthName);
-                        await expect(signUpPopup.nameLengthValidationError).toBeHidden();
+                        await expect(signUpPopup.nameValidationError).toBeHidden();
             
                         await signUpPopup.nameInput.fill(testData.maximumLengthName);
-                        await expect(signUpPopup.nameLengthValidationError).toBeHidden();
+                        await expect(signUpPopup.nameValidationError).toBeHidden();
             
                         await signUpPopup.nameInput.fill(testData.longName);
-                        await expect(signUpPopup.nameLengthValidationError).toBeVisible();
+                        await expect(signUpPopup.nameValidationError).toHaveText('Name has to be from 2 to 20 characters long');
                         await expect(signUpPopup.nameInput).toHaveCSS('border-color', testData.errorBorderColorCSS);
+                        await expect(signUpPopup.registerBtn, 'Register button should be disabled').toBeDisabled();
                     });
                 });
             
@@ -90,59 +97,65 @@ test.describe('Auth', () => {
                     await test.step('Empty Last name field validation', async () => {
                         await signUpPopup.lastNameInput.focus();
                         await signUpPopup.lastNameInput.blur();
-                        await expect(signUpPopup.emptyLastNameError).toBeVisible();
+                        await expect(signUpPopup.lastNameValidationError).toHaveText('Last name required');
                         await expect(signUpPopup.lastNameInput).toHaveCSS('border-color', testData.errorBorderColorCSS);
+                        await expect(signUpPopup.registerBtn, 'Register button should be disabled').toBeDisabled();
             
                         await signUpPopup.lastNameInput.fill(testData.testLastName);
-                        await expect(signUpPopup.emptyLastNameError).toBeHidden();
+                        await expect(signUpPopup.lastNameValidationError).toBeHidden();
                     });
             
                     await test.step('Invalid Last name validation', async () => {
                         await signUpPopup.lastNameInput.fill(testData.invalidLastName);
-                        await expect(signUpPopup.invalidLastNameError).toBeVisible();
+                        await expect(signUpPopup.lastNameValidationError).toHaveText('Last name is invalid');
                         await expect(signUpPopup.lastNameInput).toHaveCSS('border-color', testData.errorBorderColorCSS);
+                        await expect(signUpPopup.registerBtn, 'Register button should be disabled').toBeDisabled();
             
                         await signUpPopup.lastNameInput.fill(testData.testLastName);
-                        await expect(signUpPopup.invalidLastNameError).toBeHidden();
+                        await expect(signUpPopup.lastNameValidationError).toBeHidden();
                     });
             
                     await test.step('Invalid length validation', async () => {
                         await signUpPopup.lastNameInput.fill(testData.shortLastName);
-                        await expect(signUpPopup.lastNameLengthValidationError).toBeVisible();
+                        await expect(signUpPopup.lastNameValidationError).toHaveText('Last name has to be from 2 to 20 characters long');
                         await expect(signUpPopup.lastNameInput).toHaveCSS('border-color', testData.errorBorderColorCSS);
+                        await expect(signUpPopup.registerBtn, 'Register button should be disabled').toBeDisabled();
             
                         await signUpPopup.lastNameInput.fill(testData.minimumLengthLastName);
-                        await expect(signUpPopup.lastNameLengthValidationError).toBeHidden();
+                        await expect(signUpPopup.lastNameValidationError).toBeHidden();
             
                         await signUpPopup.lastNameInput.fill(testData.maximumLengthLastName);
-                        await expect(signUpPopup.lastNameLengthValidationError).toBeHidden();
+                        await expect(signUpPopup.lastNameValidationError).toBeHidden();
             
                         await signUpPopup.lastNameInput.fill(testData.longLastName);
-                        await expect(signUpPopup.lastNameLengthValidationError).toBeVisible();
+                        await expect(signUpPopup.lastNameValidationError).toHaveText('Last name has to be from 2 to 20 characters long');
                         await expect(signUpPopup.lastNameInput).toHaveCSS('border-color', testData.errorBorderColorCSS);
+                        await expect(signUpPopup.registerBtn, 'Register button should be disabled').toBeDisabled();
                     });
                 });
             
-                test('Email input field validation', async ({page}) => {
+                test('Email input field validation', async () => {
                     await test.step('Empty Email field validation', async ()=>{
                         await signUpPopup.emailInput.focus();
                         await signUpPopup.emailInput.blur();
-                        await expect(signUpPopup.emptyEmailError).toBeVisible();
+                        await expect(signUpPopup.emailValidationError).toHaveText('Email required');
                         await expect(signUpPopup.emailInput).toHaveCSS('border-color', testData.errorBorderColorCSS);
+                        await expect(signUpPopup.registerBtn, 'Register button should be disabled').toBeDisabled();
             
                         await signUpPopup.emailInput.fill(testData.testEmail);
-                        await expect(signUpPopup.emptyEmailError).toBeHidden();
+                        await expect(signUpPopup.emailValidationError).toBeHidden();
                     });
             
                     await test.step('Incorrect Email validation', async ()=>{
                         for (let i = 0; i < testData.invalidEmails.length; i++) {
                             await signUpPopup.emailInput.fill(testData.invalidEmails[i]);
-                            await expect(signUpPopup.invalidEmailError).toBeVisible();
+                            await expect(signUpPopup.emailValidationError).toHaveText('Email is incorrect');
                             await expect(signUpPopup.emailInput).toHaveCSS('border-color', testData.errorBorderColorCSS);
+                            await expect(signUpPopup.registerBtn, 'Register button should be disabled').toBeDisabled();
                         }
                 
                         await signUpPopup.emailInput.fill(testData.testEmail);
-                        await expect(signUpPopup.invalidEmailError).toBeHidden();
+                        await expect(signUpPopup.emailValidationError).toBeHidden();
                     });
                 });
                 
@@ -150,22 +163,24 @@ test.describe('Auth', () => {
                     await test.step('Empty Password field validation', async ()=>{
                         await signUpPopup.passwordInput.focus();
                         await signUpPopup.passwordInput.blur();
-                        await expect(signUpPopup.emptyPasswordError).toBeVisible();
+                        await expect(signUpPopup.passwordValidationError).toHaveText('Password required');
                         await expect(signUpPopup.passwordInput).toHaveCSS('border-color', testData.errorBorderColorCSS);
+                        await expect(signUpPopup.registerBtn, 'Register button should be disabled').toBeDisabled();
             
                         await signUpPopup.passwordInput.fill(testData.testPassword);
-                        await expect(signUpPopup.emptyPasswordError).toBeHidden();
+                        await expect(signUpPopup.passwordValidationError).toBeHidden();
                     });
                     
                     await test.step('Invalid Password field validation', async ()=>{
                         for (let i = 0; i < testData.invalidPasswords.length; i++) {
                             await signUpPopup.passwordInput.fill(testData.invalidPasswords[i]);
-                            await expect(signUpPopup.invalidPasswordError).toBeVisible();
+                            await expect(signUpPopup.passwordValidationError).toHaveText('Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter');
                             await expect(signUpPopup.passwordInput).toHaveCSS('border-color', testData.errorBorderColorCSS);
+                            await expect(signUpPopup.registerBtn, 'Register button should be disabled').toBeDisabled();
                         }
                 
                         await signUpPopup.passwordInput.fill(testData.testPassword);
-                        await expect(signUpPopup.invalidPasswordError).toBeHidden();
+                        await expect(signUpPopup.passwordValidationError).toBeHidden();
                     });
                 });
                 
@@ -173,22 +188,24 @@ test.describe('Auth', () => {
                     await test.step('Empty Re-enter password field validation', async () => {
                         await signUpPopup.reenterPasswordInput.focus();
                         await signUpPopup.reenterPasswordInput.blur();
-                        await expect(signUpPopup.emptyReenterPasswordError).toBeVisible();
+                        await expect(signUpPopup.reenterPasswordValidationError).toHaveText('Re-enter password required');
                         await expect(signUpPopup.reenterPasswordInput).toHaveCSS('border-color', testData.errorBorderColorCSS);
+                        await expect(signUpPopup.registerBtn, 'Register button should be disabled').toBeDisabled();
             
                         await signUpPopup.reenterPasswordInput.fill(testData.testPassword);
-                        await expect(signUpPopup.emptyReenterPasswordError).toBeHidden();
+                        await expect(signUpPopup.reenterPasswordValidationError).toHaveText('Passwords do not match');
                     });
                     
                     await test.step('Invalid Re-enter password field validation', async ()=>{
                         for (let i = 0; i < testData.invalidPasswords.length; i++) {
                             await signUpPopup.reenterPasswordInput.fill(testData.invalidPasswords[i]);
-                            await expect(signUpPopup.invalidReenterPasswordError).toBeVisible();
+                            await expect(signUpPopup.reenterPasswordValidationError).toHaveText('Password has to be from 8 to 15 characters long and contain at least one integer, one capital, and one small letter');
                             await expect(signUpPopup.reenterPasswordInput).toHaveCSS('border-color', testData.errorBorderColorCSS);
+                            await expect(signUpPopup.registerBtn, 'Register button should be disabled').toBeDisabled();
                         }
                 
                         await signUpPopup.reenterPasswordInput.fill(testData.testPassword);
-                        await expect(signUpPopup.invalidReenterPasswordError).toBeHidden();
+                        await expect(signUpPopup.reenterPasswordValidationError).toHaveText('Passwords do not match');
                     });
                 });
             });
